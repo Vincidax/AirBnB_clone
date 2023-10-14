b94399786505def8e72b0f8f558e46a6681ec57e
Vincidax <vincentdx18@gmail.com>
2023-10-14 10:34:52 -0700

    test package

---
+++ tests/test_engine/__init__.py
@@ -0,0 +1,1 @@
+all = [" test_file_storage.py"]
---
+++ tests/test_models/__init__.py
@@ -0,0 +1,9 @@
+all = [
+    "test_base_model.py",
+    "test_place.py",
+    "test_state.py",
+    "test_amenity.py",
+    "test_city.py",
+    "test_review.py",
+    "test_user.py"
+]
