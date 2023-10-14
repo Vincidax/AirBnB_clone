1f9cbd63e7f16ae5581a807e6e72dbc4156c9d82
Vincidax <vincentdx18@gmail.com>
2023-10-14 07:32:03 -0700

    test_file_storage

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,55 @@
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        """Set up for the tests"""
+        self.file_storage = FileStorage()
+        self.base_model = BaseModel()
+
+    def tearDown(self):
+        """Tear down for the tests"""
+        del self.file_storage
+        del self.base_model
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
+    def test_all(self):
+        """Test the all method"""
+        self.assertIsInstance(self.file_storage.all(), dict)
+
+    def test_new(self):
+        """Test the new method"""
+        self.file_storage.new(self.base_model)
+        key = "{}.{}".format(type(self.base_model).__name__, self.base_model.id)
+        self.assertIn(key, self.file_storage.all())
+
+    def test_save(self):
+        """Test the save method"""
+        self.file_storage.new(self.base_model)
+        self.file_storage.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_reload(self):
+        """Test the reload method"""
+        self.file_storage.new(self.base_model)
+        self.file_storage.save()
+        self.file_storage.reload()
+        key = "{}.{}".format(type(self.base_model).__name__, self.base_model.id)
+        self.assertIn(key, self.file_storage.all())
+
+    def test_new_none(self):
+        """Test the new method with None"""
+        with self.assertRaises(AttributeError):
+            self.file_storage.new(None)
+
+    def test_reload_no_file(self):
+        """Test the reload method with no file"""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+        self.file_storage.reload()  # Should not raise
+
+if __name__ == '__main__':
+    unittest.main()
