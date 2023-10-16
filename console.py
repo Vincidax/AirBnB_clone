f820c4e70887ba8a5bbab3f6583b09a965d73193
Vincent DUSHIME <vincentdx18@gmail.com>
2023-10-16 07:46:41 +0200

    emptyline

--- console.py
+++ console.py
@@ -157,6 +157,12 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program"""
         return True
 
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered.
+        """
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
